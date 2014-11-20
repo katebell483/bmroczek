47d9c9dfe19b7cba937d93c0adc1dec93ddc051b
root <root@bmroczek.(none)>
2014-11-20 01:36:12 +0000

    new changes

--- wp-content/themes/bmroczek/functions.php
+++ wp-content/themes/bmroczek/functions.php
@@ -64,3 +64,16 @@ return count( $comments_by_type['comment'] );
 return $count;
 }
 }
+
+add_filter( 'the_content', 'attachment_image_link_remove_filter' );
+function attachment_image_link_remove_filter( $content ) {
+ 	$content =
+		 preg_replace(
+		 array('{<a(.*?)(wp-att|wp-content/uploads)[^>]*><img}',
+		 '{ wp-image-[0-9]*" /></a>}'),
+		 array('<img','" />'),
+		 $content
+	 	);
+ 	return $content;
+ }
+
